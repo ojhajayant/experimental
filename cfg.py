be1514aea5833cf7dd660e23b3267a4df7a01739
JayO <45680293+ojhajayant@users.noreply.github.com>
2023-02-14 22:03:21 +0530

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -50,7 +50,7 @@ def get_args():
                         help='lr lower range value used for the LR-range-test')
     parser.add_argument('--end_lr', default=1, type=float,
                         help='lr upper range value used for the LR-range-test')
-    parser.add_argument('--max_lr_epochs', '-e', default=5, type=int,
+    parser.add_argument('--max_lr_epochs', '-M', default=5, type=int,
                         help='at what epoch Max LR should reach?')
     parser.add_argument('--lr_range_test_epochs', '-E', default=500, type=int,
                         help='epoch value used for the LR-range-test')
