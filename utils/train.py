fbbf034ad3bc3390994de8def6cff8d0a5a28786
JayO <45680293+ojhajayant@users.noreply.github.com>
2023-02-16 03:57:45 +0530

    Update train.py

--- utils/train.py
+++ utils/train.py
@@ -77,3 +77,53 @@ def train(model, device, train_loader, optimizer, epoch, criterion,
             desc=f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100 * correct / processed:0.2f}')
     cfg.train_acc.append(100 * correct / processed)
     return (100 * correct / processed)
+
+
+def train_calc_acc(model, device, train_iter, optimizer, criterion,
+          scheduler=None, L1=False):
+    """
+    main training code
+    """
+    global args
+    model.train()
+    correct = 0
+    processed = 0
+    data, target = next(train_iter)
+    # get samples
+    data, target = data.to(device), target.to(device)
+    # Init
+    optimizer.zero_grad()
+    # In PyTorch, we need to set the gradients to zero before starting to
+    # do back-propagation because PyTorch accumulates the gradients on
+    # subsequent backward passes. Because of this, when you start your
+    # training loop, ideally you should zero out the gradients so that
+    # you do the parameter update correctly.
+
+    # Predict
+    y_pred = model(data)
+    if L1:
+        to_reg = []
+        for param in model.parameters():
+            to_reg.append(param.view(-1))
+        l1 = args.l1_weight * misc.l1_penalty(torch.cat(to_reg))
+    else:
+        l1 = 0
+    # Calculate loss
+    # L1 regularization adds an L1 penalty equal to the
+    # absolute value of the magnitude of coefficients
+    # torch.nn.CrossEntropyLoss:criterion combines
+    # nn.LogSoftmax() and nn.NLLLoss() in one single class.
+    loss = criterion(y_pred, target) + l1
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+    if scheduler is not None:
+        scheduler.step()
+    # Update pbar-tqdm
+    pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max
+    # log-probability
+    correct += pred.eq(target.view_as(pred)).sum().item()
+    processed += len(data)
+    cfg.train_acc_calc.append(100 * correct / processed)
+    return 100 * correct / processed
+
