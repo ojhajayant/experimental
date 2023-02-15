01ada00340555ba5b03b416354d001bd50848dfd
JayO <45680293+ojhajayant@users.noreply.github.com>
2023-02-16 01:43:09 +0530

    Update lr_finder.py

--- utils/lr_finder.py
+++ utils/lr_finder.py
@@ -362,8 +362,8 @@ class LRFinder(object):
             end_lr=10,
             num_epochs=100,
             step_mode="linear",
-            smooth_f=0.05,
-            diverge_th=5,
+            smooth_f=0,
+            # smooth_f=0.05,
             accumulation_steps=1,
             non_blocking_transfer=True,
     ):
@@ -445,19 +445,19 @@ class LRFinder(object):
         # Set the starting learning rate
         if start_lr:
             self._set_learning_rate(start_lr)
-
+        num_iter = len([b for b, _ in enumerate(train_loader)])
         # Initialize the proper learning rate policy
         if step_mode.lower() == "exp":
-            lr_schedule = ExponentialLR(self.optimizer, end_lr, num_epochs)
+            lr_schedule = ExponentialLR(self.optimizer, end_lr, num_iter*num_epochs)
         elif step_mode.lower() == "linear":
-            lr_schedule = LinearLR(self.optimizer, end_lr, num_epochs)
+            lr_schedule = LinearLR(self.optimizer, end_lr, num_iter*num_epochs)
         else:
             raise ValueError(
                 "expected one of (exp, linear), got {}".format(step_mode))
 
         if smooth_f < 0 or smooth_f >= 1:
             raise ValueError("smooth_f is outside the range [0, 1[")
-        num_iter = len([b for b, _ in enumerate(train_loader)])
+
         for epoch in tqdm(range(num_epochs)):
             # Create an iterator to get data batch by batch
             if isinstance(train_loader, DataLoader):
@@ -616,112 +616,111 @@ class LRFinder(object):
 
         return running_loss / len(val_iter.dataset)
 
-        def plot(
-                self,
-                skip_start=10,
-                skip_end=5,
-                log_lr=True,
-                show_lr=None,
-                ax=None,
-                suggest_lr=False,
-                xaxis_label="Learning rate",
-                yaxis_label="Loss"
-        ):
-            """Plots the learning rate range test.
-            Arguments:
-                skip_start (int, optional): number of batches to trim from the start.
-                    Default: 10.
-                skip_end (int, optional): number of batches to trim from the start.
-                    Default: 5.
-                log_lr (bool, optional): True to plot the learning rate in a logarithmic
-                    scale; otherwise, plotted in a linear scale. Default: True.
-                show_lr (float, optional): if set, adds a vertical line to visualize the
-                    specified learning rate. Default: None.
-                ax (matplotlib.axes.Axes, optional): the plot is created in the specified
-                    matplotlib axes object and the figure is not be shown. If `None`, then
-                    the figure and axes object are created in this method and the figure is
-                    shown . Default: None.
-                suggest_lr (bool, optional): suggest a learning rate by
-                    - 'steepest': the point with steepest gradient (minimal gradient)
-                    you can use that point as a first guess for an LR. Default: True.
-            Returns:
-                The matplotlib.axes.Axes object that contains the plot,
-                and the suggested learning rate (if set suggest_lr=True).
-                :param yaxis_label: 
-                :param xaxis_label: 
-            """
+    def plot(
+            self,
+            skip_start=10,
+            skip_end=5,
+            log_lr=True,
+            show_lr=None,
+            ax=None,
+            suggest_lr=False,
+            xaxis_label="Learning rate",
+            yaxis_label="Loss"
+    ):
+        """Plots the learning rate range test.
+        Arguments:
+            skip_start (int, optional): number of batches to trim from the start.
+                Default: 10.
+            skip_end (int, optional): number of batches to trim from the start.
+                Default: 5.
+            log_lr (bool, optional): True to plot the learning rate in a logarithmic
+                scale; otherwise, plotted in a linear scale. Default: True.
+            show_lr (float, optional): if set, adds a vertical line to visualize the
+                specified learning rate. Default: None.
+            ax (matplotlib.axes.Axes, optional): the plot is created in the specified
+                matplotlib axes object and the figure is not be shown. If `None`, then
+                the figure and axes object are created in this method and the figure is
+                shown . Default: None.
+            suggest_lr (bool, optional): suggest a learning rate by
+                - 'steepest': the point with steepest gradient (minimal gradient)
+                you can use that point as a first guess for an LR. Default: True.
+        Returns:
+            The matplotlib.axes.Axes object that contains the plot,
+            and the suggested learning rate (if set suggest_lr=True).
+            :param yaxis_label:
+            :param xaxis_label:
+        """
 
-            if skip_start < 0:
-                raise ValueError("skip_start cannot be negative")
-            if skip_end < 0:
-                raise ValueError("skip_end cannot be negative")
-            if show_lr is not None and not isinstance(show_lr, float):
-                raise ValueError("show_lr must be float")
+        if skip_start < 0:
+            raise ValueError("skip_start cannot be negative")
+        if skip_end < 0:
+            raise ValueError("skip_end cannot be negative")
+        if show_lr is not None and not isinstance(show_lr, float):
+            raise ValueError("show_lr must be float")
 
-            # Get the data to plot from the history dictionary. Also, handle skip_end=0
-            # properly so the behaviour is the expected
-            lrs = self.history["lr"]
-            losses = self.history["loss"]
-            if skip_end == 0:
-                lrs = lrs[skip_start:]
-                losses = losses[skip_start:]
-            else:
-                lrs = lrs[skip_start:-skip_end]
-                losses = losses[skip_start:-skip_end]
+        # Get the data to plot from the history dictionary. Also, handle skip_end=0
+        # properly so the behaviour is the expected
+        lrs = self.history["lr"]
+        losses = self.history["acc"]
+        if skip_end == 0:
+            lrs = lrs[skip_start:]
+            losses = losses[skip_start:]
+        else:
+            lrs = lrs[skip_start:-skip_end]
+            losses = losses[skip_start:-skip_end]
 
-            # Create the figure and axes object if axes was not already given
-            fig = None
-            if ax is None:
-                fig, ax = plt.subplots()
+        # Create the figure and axes object if axes was not already given
+        fig = None
+        if ax is None:
+            fig, ax = plt.subplots()
 
-            # Plot loss as a function of the learning rate
-            ax.plot(lrs, losses)
+        # Plot loss as a function of the learning rate
+        ax.plot(lrs, losses)
 
-            # Plot the suggested LR
-            if suggest_lr:
-                # 'steepest': the point with steepest gradient (minimal gradient)
-                print("LR suggestion: steepest gradient")
-                min_grad_idx = None
-                try:
-                    min_grad_idx = (np.gradient(np.array(losses))).argmin()
-                except ValueError:
-                    print(
-                        "Failed to compute the gradients, there might not be enough points."
-                    )
-                if min_grad_idx is not None:
-                    print("Suggested LR: {:.2E}".format(lrs[min_grad_idx]))
-                    ax.scatter(
-                        lrs[min_grad_idx],
-                        losses[min_grad_idx],
-                        s=75,
-                        marker="o",
-                        color="red",
-                        zorder=3,
-                        label="steepest gradient",
-                    )
-                    ax.legend()
+        # Plot the suggested LR
+        if suggest_lr:
+            # 'steepest': the point with steepest gradient (minimal gradient)
+            print("LR suggestion: steepest gradient")
+            min_grad_idx = None
+            try:
+                min_grad_idx = (np.gradient(np.array(losses))).argmin()
+            except ValueError:
+                print(
+                    "Failed to compute the gradients, there might not be enough points."
+                )
+            if min_grad_idx is not None:
+                print("Suggested LR: {:.2E}".format(lrs[min_grad_idx]))
+                ax.scatter(
+                    lrs[min_grad_idx],
+                    losses[min_grad_idx],
+                    s=75,
+                    marker="o",
+                    color="red",
+                    zorder=3,
+                    label="steepest gradient",
+                )
+                ax.legend()
 
-            if log_lr:
-                ax.set_xscale("log")
-            ax.set_xlabel(xaxis_label)
-            ax.set_ylabel(yaxis_label)
+        if log_lr:
+            ax.set_xscale("log")
+        ax.set_xlabel(xaxis_label)
+        ax.set_ylabel(yaxis_label)
 
-            if show_lr is not None:
-                ax.axvline(x=show_lr, color="red")
+        if show_lr is not None:
+            ax.axvline(x=show_lr, color="red")
 
-            # Show only if the figure was created internally
-            if fig is not None:
-                plt.savefig('plot13.png')
-                from IPython.display import Image
-                Image(filename='plot13.png')
-                display(plt.gcf())
-                plt.show()
+        # Show only if the figure was created internally
+        if fig is not None:
+            plt.savefig('plot13.png')
+            from IPython.display import Image
+            Image(filename='plot13.png')
+            display(plt.gcf())
+            plt.show()
 
-            if suggest_lr and min_grad_idx is not None:
-                return ax, lrs[min_grad_idx]
-            else:
-                return ax
-
+        if suggest_lr and min_grad_idx is not None:
+            return ax, lrs[min_grad_idx]
+        else:
+            return ax
 
 class LinearLR(_LRScheduler):
     """Linearly increases the learning rate between two boundaries over a number of
