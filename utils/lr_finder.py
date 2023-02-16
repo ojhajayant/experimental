5afef763ac71a64797f46385aca4617448535b16
JayO <45680293+ojhajayant@users.noreply.github.com>
2023-02-16 11:28:00 +0530

    Update lr_finder.py

--- utils/lr_finder.py
+++ utils/lr_finder.py
@@ -849,20 +849,80 @@ class StateCacher(object):
                 os.remove(self.cached[k])
 
 
+# def find_network_lr(model, criterion, optimizer, device, train_loader, init_lr,
+#                     init_weight_decay, end_lr=1, num_epochs=100):
+#     print(
+#         f"Finding max LR for One Cycle Policy using LR Range Test over {num_epochs} epochs...")
+#     lr_range_test_optimizer = optimizer(model.parameters(), lr=init_lr,
+#                                         weight_decay=init_weight_decay)
+#     lr_finder = LRFinder(model, lr_range_test_optimizer, criterion,
+#                          device=device)
+#     lr_finder.range_test_acc_over_epochs(train_loader, end_lr=end_lr,
+#                                          num_epochs=num_epochs)
+#     max_val_index = lr_finder.history['acc'].index(lr_finder.best_acc)
+#     best_lr = lr_finder.history['lr'][max_val_index]
+#     print(f"LR (max accuracy {lr_finder.best_acc}) to be used: {best_lr}")
+#     lr_finder.plot(show_lr=best_lr,
+#                    yaxis_label="Training Accuracy")  # to inspect the accuracy-learning rate graph
+#     lr_finder.reset()  # to reset the self.model and optimizer to their initial state
+#     return best_lr
+
+
+# !/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+import copy
+
+Lrtest_train_acc = []
+LRtest_Lr = []
+
+
+def LR_test(max_lr, min_lr, device, epoch, model, criterion, trainloader,
+            momemtum=0.9, weight_decay=0.05, plot=True):
+    step = (max_lr - min_lr) / epoch
+    lr = min_lr
+    for e in range(epoch):
+        testmodel = copy.deepcopy(model)
+        optimizer = optim.SGD(testmodel.parameters(), lr=lr, momentum=momemtum,
+                              weight_decay=weight_decay)
+        lr += (max_lr - min_lr) / epoch
+        testmodel.train()
+        pbar = tqdm(trainloader)
+        correct = 0
+        processed = 0
+        for batch_idx, (data, target) in enumerate(pbar):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            y_pred = testmodel(data)
+            loss = criterion(y_pred, target)
+            loss.backward()
+            optimizer.step()
+            pred = y_pred.argmax(dim=1,
+                                 keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            processed += len(data)
+            pbar.set_description(
+                desc=f'epoch = {e + 1} Lr = {optimizer.param_groups[0]["lr"]}  Loss={loss.item()} Batch_id={batch_idx} Accuracy={100 * correct / processed:0.2f}')
+        Lrtest_train_acc.append(100 * correct / processed)
+        LRtest_Lr.append(optimizer.param_groups[0]['lr'])
+
+    if plot:
+        plt.plot(LRtest_Lr, Lrtest_train_acc)
+        plt.ylabel('train Accuracy')
+        plt.xlabel("Learning rate")
+        plt.title("Lr v/s accuracy")
+        plt.show()
+
+
 def find_network_lr(model, criterion, optimizer, device, train_loader, init_lr,
                     init_weight_decay, end_lr=1, num_epochs=100):
-    print(
-        f"Finding max LR for One Cycle Policy using LR Range Test over {num_epochs} epochs...")
-    lr_range_test_optimizer = optimizer(model.parameters(), lr=init_lr,
-                                        weight_decay=init_weight_decay)
-    lr_finder = LRFinder(model, lr_range_test_optimizer, criterion,
-                         device=device)
-    lr_finder.range_test_acc_over_epochs(train_loader, end_lr=end_lr,
-                                         num_epochs=num_epochs)
-    max_val_index = lr_finder.history['acc'].index(lr_finder.best_acc)
-    best_lr = lr_finder.history['lr'][max_val_index]
-    print(f"LR (max accuracy {lr_finder.best_acc}) to be used: {best_lr}")
-    lr_finder.plot(show_lr=best_lr,
-                   yaxis_label="Training Accuracy")  # to inspect the accuracy-learning rate graph
-    lr_finder.reset()  # to reset the self.model and optimizer to their initial state
-    return best_lr
+    LR_test(end_lr, init_lr, device, num_epochs, model, criterion, train_loader,
+            momemtum=0.9, weight_decay=0.05, plot=True)
